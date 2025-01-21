5320589af08f0de793d4fa5b946ffb0494f5198f
Theodor Radu <theo.registrations@icloud.com>
2025-01-21 15:34:03 +0100

    QW-8: Implement Fight Service

--- super-heroes/rest-villains/src/test/java/io/quarkus/workshop/superheroes/villain/VillainResourceIT.java
+++
@@ -1,8 +0,0 @@
-package io.quarkus.workshop.superheroes.villain;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class VillainResourceIT extends VillainResourceTest {
-    // Execute the same tests but in packaged mode.
-}
